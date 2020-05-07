038ae60e8a3f125657150d980388fdd32a88be89
samsadch <samsadch@gmail.com>
2020-05-07 12:56:45 +0400

    iPhone constraints added

---
+++ dev-profile/dev-profile/View/RoundCornerImage.swift
@@ -0,0 +1,17 @@
+//
+//  RoundCornerImage.swift
+//  dev-profile
+//
+//  Created by Samsad CV on 07/05/20.
+//  Copyright © 2020 Samsad CV. All rights reserved.
+//
+
+import UIKit
+
+class RoundCornerImage: UIImage {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+}
