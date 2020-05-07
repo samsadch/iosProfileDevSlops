1a9b62d2204e4f159b4f83b1a0a3d9009c193471
samsadch <samsadch@gmail.com>
2020-05-07 11:54:42 +0400

    Initial XCode Project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Samsad CV on 07/05/20.
+//  Copyright © 2020 Samsad CV. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
